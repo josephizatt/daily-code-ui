3564ce01e3c7d50c011b73bad1ad5553e7e9707e
Joseph Izatt <josephizatt@Josephs-MacBook-Air.local>
2019-01-18 10:23:06 +0000

    Add form script

--- 01/script.js
+++ 01/script.js
@@ -1,3 +1,23 @@
+var form = document.getElementById('form');
+
+// Adds a listener for the "submit" event.
+form.addEventListener('submit', function(e) {
+
+  e.preventDefault();
+
+});
+
+function validateForm() {
+    var list = document.forms["form"]["list"].value;
+    var k = document.forms["form"]["k"].value;
+    if (list == "") {
+      alert("List must be filled out");
+      return false;
+    }
+    document.getElementById("result").innerHTML = "<p>" + two_sum(list, k) + "</p>";
+}
+
+
 let two_sum = function(list, k) {
     for(var i = 0; i < list.length; i++) {
         for(var j =0; j < list.length; j++) {
